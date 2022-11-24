7e717c6aa7d3f8a337e8c9ec0f70e024e712a1cb
Piotr Wilkosz <pwilkosz99@gmail.com>
2022-11-25 00:30:19 +0100

    Rules

--- Project/FuzzyTrainControlSystem/FuzzyTrainControlSystem/MainPage.xaml.cs
+++ Project/FuzzyTrainControlSystem/FuzzyTrainControlSystem/MainPage.xaml.cs
@@ -33,32 +33,91 @@ namespace FuzzyTrainControlSystem
 
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            var water = new LinguisticVariable("Water");
-            var cold = water.MembershipFunctions.AddTrapezoid("Cold", 0, 0, 20, 40);
-            var warm = water.MembershipFunctions.AddTriangle("Warm", 30, 50, 70);
-            var hot = water.MembershipFunctions.AddTrapezoid("Hot", 50, 80, 100, 100);
+            var dlugosc_hamowania = new LinguisticVariable("Dlugosc_hamowania");
+            var dh_krotka = dlugosc_hamowania.MembershipFunctions.AddTrapezoid("Krotka", 0, 0, 150, 400);
+            var dh_srednia = dlugosc_hamowania.MembershipFunctions.AddTriangle("Srednia", 250, 500, 800);
+            var dh_dluga = dlugosc_hamowania.MembershipFunctions.AddTrapezoid("Dluga", 600, 900, 1000, 1000);
 
-            var power = new LinguisticVariable("Power");
-            var low = power.MembershipFunctions.AddTriangle("Low", 0, 25, 50);
-            var high = power.MembershipFunctions.AddTriangle("High", 25, 50, 75);
+            var predkosc_poprzedzajacego = new LinguisticVariable("Predkosc_poprzedzajacego");
+            var pp_niska = predkosc_poprzedzajacego.MembershipFunctions.AddTrapezoid("Niska", 0, 0, 500, 700);
+            var pp_srednia = predkosc_poprzedzajacego.MembershipFunctions.AddTriangle("Srednia", 55, 95, 135);
+            var pp_wysoka = predkosc_poprzedzajacego.MembershipFunctions.AddTrapezoid("Wysoka", 120, 165, 200, 200);
+
+            var odleglosc_poprzedzajacego = new LinguisticVariable("Odleglosc_poprzedzajacego");
+            var od_niska = odleglosc_poprzedzajacego.MembershipFunctions.AddTrapezoid("Niska", 0, 0, 500, 750);
+            var od_srednia = odleglosc_poprzedzajacego.MembershipFunctions.AddTrapezoid("Srednia", 650, 1000, 1750, 2200);
+            var od_duza = odleglosc_poprzedzajacego.MembershipFunctions.AddTrapezoid("Duza", 1750, 2500, 3000, 3750);
+            var od_bardzo_duza = odleglosc_poprzedzajacego.MembershipFunctions.AddTrapezoid("Bardzo duza", 3500, 4000, 5000, 5000);
+
+            var predkosc_pojazdu = new LinguisticVariable("Predkosc_pojazdu");
+            var pr_stop = predkosc_pojazdu.MembershipFunctions.AddTriangle("Stop", 0, 0, 0);
+            var pr_niska = predkosc_pojazdu.MembershipFunctions.AddTrapezoid("Niska", 0, 0, 30, 50);
+            var pr_srednia = predkosc_pojazdu.MembershipFunctions.AddTrapezoid("Srednia", 30, 50, 70, 90);
+            var pr_wysoka = predkosc_pojazdu.MembershipFunctions.AddTrapezoid("Wysoka", 80, 100, 140, 160);
+            var pr_bardzo_wysoka = predkosc_pojazdu.MembershipFunctions.AddTrapezoid("Bardzo Wysoka", 140, 160, 200, 200);
 
             IFuzzyEngine fuzzyEngine = new FuzzyEngineFactory().Default();
 
-            var rule1 = Rule.If(water.Is(cold).And(water.Is(warm))).Then(power.Is(high));
-            var rule2 = Rule.If(water.Is(hot)).Then(power.Is(low));
-            fuzzyEngine.Rules.Add(rule1, rule2);
+            FuzzyRule[] rules =
+            {
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_niska))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_niska))).Then(predkosc_pojazdu.Is(pr_stop)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_niska))).Then(predkosc_pojazdu.Is(pr_stop)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_niska))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_niska))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_niska))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_niska))).Then(predkosc_pojazdu.Is(pr_srednia)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_niska))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_niska))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_srednia))).Then(predkosc_pojazdu.Is(pr_srednia)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_srednia))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_srednia))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_srednia))).Then(predkosc_pojazdu.Is(pr_srednia)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_srednia))).Then(predkosc_pojazdu.Is(pr_srednia)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_srednia))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_srednia))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_srednia))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_srednia))).Then(predkosc_pojazdu.Is(pr_srednia)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_duza))).Then(predkosc_pojazdu.Is(pr_srednia)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_duza))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_duza))).Then(predkosc_pojazdu.Is(pr_niska)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_duza))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_duza))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_duza))).Then(predkosc_pojazdu.Is(pr_srednia)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_duza))).Then(predkosc_pojazdu.Is(pr_bardzo_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_duza))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_duza))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_bardzo_duza))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_bardzo_duza))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_niska)).And(odleglosc_poprzedzajacego.Is(od_bardzo_duza))).Then(predkosc_pojazdu.Is(pr_srednia)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_bardzo_duza))).Then(predkosc_pojazdu.Is(pr_bardzo_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_bardzo_duza))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_srednia)).And(odleglosc_poprzedzajacego.Is(od_bardzo_duza))).Then(predkosc_pojazdu.Is(pr_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_krotka).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_bardzo_duza))).Then(predkosc_pojazdu.Is(pr_bardzo_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_srednia).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_bardzo_duza))).Then(predkosc_pojazdu.Is(pr_bardzo_wysoka)),
+                Rule.If(dlugosc_hamowania.Is(dh_dluga).And(predkosc_poprzedzajacego.Is(pp_wysoka)).And(odleglosc_poprzedzajacego.Is(od_bardzo_duza))).Then(predkosc_pojazdu.Is(pr_wysoka))
+            };
 
-            var result = fuzzyEngine.Defuzzify(new { water = 60 });
+            fuzzyEngine.Rules.Add(rules);
+
+            var result = fuzzyEngine.Defuzzify(new { Dlugosc_hamowania = 500.0, Predkosc_poprzedzajacego = 100.0, Odleglosc_poprzedzajacego = 5000.0 });
+
+            tb_result.Text = result.ToString();
         }
 
         private void btn_calculate_Click(object sender, RoutedEventArgs e)
         {
-            tb_result.Text = txt_weight.Text;
+            //tb_result.Text = txt_weight.Text;
             Debug.WriteLine(txt_weight.Text);
             Debug.WriteLine(txt_brake_weight_perc.Text);
             Debug.WriteLine(txt_humidity.Text);
             Debug.WriteLine(txt_predecessor_distance.Text);
             Debug.WriteLine(txt_predecessor_speed.Text);
+        }
+
+        public int calculateBreakingRange()
+        {
+            return 1;
         }
     }
 }
